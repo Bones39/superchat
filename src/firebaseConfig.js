b6f904fc3d499a22ce4ecc96af97441b05467a96
Andraina <asz_andraina@yahoo.fr>
2024-12-26 13:46:04 +0100

    initialize firebase config

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB6ZgSFq7KrhGRTLoNMRyy9bOibXbrwGuE",
+  authDomain: "superchat-1043c.firebaseapp.com",
+  projectId: "superchat-1043c",
+  storageBucket: "superchat-1043c.firebasestorage.app",
+  messagingSenderId: "947031551209",
+  appId: "1:947031551209:web:2e332ed4ea3a77701b9478"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
