e233ec697d84d1a0e41986252e005385183e435b
Andraina <asz_andraina@yahoo.fr>
2025-01-08 19:34:17 +0100

    style the log in page and add an image

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,11 +1,10 @@
 const LogIn = ({signIn, signInWithGoogle}) => {
 	return (
 		<div className="signIn">
-			{/* // chat room */}
-			<input type="text" placeholder='Email'/>
-			<input type="password" placeholder='password'/>
-			<button onClick={signIn}>Sign In</button>
-			<button onClick={signInWithGoogle}>Sign In with Google</button>
+			<input className="signInInput" type="text" placeholder='Email'/>
+			<input className="signInInput" type="password" placeholder='password'/>
+			<button className="signInInput" onClick={signIn}>Sign In</button>
+			<button className="signInInput" onClick={signInWithGoogle}>Sign In with Google</button>
 		</div>
 	)
 }
