07ff00e9f4f15533767e584cf06374b5eb433b65
Andraina <asz_andraina@yahoo.fr>
2025-01-09 21:45:50 +0100

    simplify the chatroom component

--- src/components/Chatroom.jsx
+++ src/components/Chatroom.jsx
@@ -8,20 +8,22 @@ const Chatroom = ({props}) => {
 			<div className="chatroom">
 				{/* // chat room */}
 				{messages && messages.map((message, index) =>
-					index !== 0 && messages[index-1].uid !== message.uid && message.uid !== auth.currentUser.uid?
-					// add a div containing the user if the message is a new message -> put this in a new component!!
-						<div key={message.id + 'div'}>
-							<div className={`${message.uid === auth.currentUser.uid ? "sent" : "received"} userTag`} key={message.id + 'tag'} style={{backgroundImage: `url("https://randomuser.me/api/portraits/men/${message.photoId}.jpg")`}}>{message.allias}</div>
-							<div className={message.uid === auth.currentUser.uid ? "sent" : "received"} key={message.id}>{message.text}</div>
-						</div>
-					:
-					index === 0 && message.uid !== auth.currentUser.uid?
-						<div key={message.id + 'div2'}>
-							<div className={`${message.uid === auth.currentUser.uid ? "sent" : "received"} userTag`}>{message.allias}</div>
-							<div className={message.uid === auth.currentUser.uid ? "sent" : "received"} key={message.id}>{message.text}</div>
-						</div>
-					:
-					<div className={message.uid === auth.currentUser.uid ? "sent" : "received"} key={message.id}>{message.text}</div>
+					{
+						let bDisplayUserPicture = (index !== 0 && messages[index-1].uid !== message.uid && message.uid !== auth.currentUser.uid)
+							|| (index === 0 && message.uid !== auth.currentUser.uid);
+						// display the picture above the message if the first message is not from the current user or if a message comes after a message which is not his
+						if (bDisplayUserPicture) {
+							return(
+								<div key={message.id + 'div'}>
+									<div className={`${message.uid === auth.currentUser.uid ? "sent" : "received"} userTag`} key={message.id + 'tag'} style={{backgroundImage: `url("https://randomuser.me/api/portraits/men/${message.photoId}.jpg")`}}>{message.allias}</div>
+									<div className={message.uid === auth.currentUser.uid ? "sent" : "received"} key={message.id}>{message.text}</div>
+								</div>
+							)
+						} else {
+							return <div className={message.uid === auth.currentUser.uid ? "sent" : "received"} key={message.id}>{message.text}</div>;
+						}
+					}
+					
 				)}
 				<div ref={dummy}></div>
 			</div>
