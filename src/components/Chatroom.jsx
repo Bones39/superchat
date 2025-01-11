d6ed7b45d1261f5c952c3b14c73230cd2efe54ec
Andraina <asz_andraina@yahoo.fr>
2025-01-11 21:02:41 +0100

    WIP add timestamp in messages

--- src/components/Chatroom.jsx
+++ src/components/Chatroom.jsx
@@ -11,14 +11,18 @@ const Chatroom = ({props}) => {
 					{
 						let bDisplayUserPicture = (index !== 0 && messages[index-1].uid !== message.uid && message.uid !== auth.currentUser.uid)
 							|| (index === 0 && message.uid !== auth.currentUser.uid);
+						const date = new Date(message.createdAt.seconds * 1000);
+						const formatedDate = `${date.toLocaleString()}`
 						// display the picture above the message if the first message is not from the current user or if a message comes after a message which is not his
-						console.log(bDisplayUserPicture);
 						if (bDisplayUserPicture) {
 							return (
 								<>
 									<div className={`${message.uid === auth.currentUser.uid ? "sent" : "received"} userTag`} key={message.id + 'tag'} style={{backgroundImage: `url("https://randomuser.me/api/portraits/men/${message.photoId}.jpg")`}}>{message.allias}</div>
-									<div className={message.uid === auth.currentUser.uid ? "sent" : "received"} key={message.id}>{message.text}</div>
-									{/* <div className={message.uid === auth.currentUser.uid ? "sent" : "received"} key={message.id + "timeStamp"}>{message.createdAt}</div> */}
+									<div className={message.uid === auth.currentUser.uid ? "sent" : "received"} key={message.id}>
+										<div>{message.text}</div>
+										<div className='timeStamp'>{formatedDate}</div>
+									</div>
+									{/* <div className={`${message.uid === auth.currentUser.uid ? "sent" : "received"} timeStamp`} key={message.id + "timeStamp"}>{formatedDate}</div> */}
 								</>
 							)
 						} else {
