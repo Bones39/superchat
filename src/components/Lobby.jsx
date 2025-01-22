b9a2ad09560f275ca9437009e086d3bfe105db42
Andraina <asz_andraina@yahoo.fr>
2025-01-22 22:42:58 +0100

    add header to the lobby component

--- src/components/Lobby.jsx
+++ src/components/Lobby.jsx
@@ -4,6 +4,7 @@ const Lobby = ({props}) => {
 	const {connected} = props;
 	return (
 		<div className="lobby">
+			<div className="lobbyHeader">Connecté(s)</div>
 			{connected && connected.map((connectedUser) =>
 				<div className="lobbyUser" key={`lobby-${connectedUser.id}`}>
 					<div className="userTagLobby" style={{backgroundImage: `url("https://randomuser.me/api/portraits/men/${connectedUser.photoId}.jpg")`, backgroundPosition: "center", backgroundSize: "110%"}}>{connectedUser.allias}</div>
