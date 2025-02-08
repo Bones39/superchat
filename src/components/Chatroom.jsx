9cb08ec01b6b1235f2350af76084d75611e6be6d
Andraina <asz_andraina@yahoo.fr>
2025-02-08 12:23:39 +0100

    WIP create a component to render messages individualy

--- src/components/Chatroom.jsx
+++ src/components/Chatroom.jsx
@@ -2,16 +2,16 @@ import { useEffect, useRef, useState } from 'react';
 import { auth } from '../firebaseConfig'
 import firebase from 'firebase/compat/app'
 import { CiImageOn } from "react-icons/ci";
+import Message from "./Message"
 import { confirmPasswordReset } from 'firebase/auth';
 import { FaLess } from 'react-icons/fa';
 
 const Chatroom = ({props}) => {
 	const {messages, formValue, sendMessage, sendImage, typing} = props;
-	const timeoutId = useRef(null);
+	const timeoutId = useRef();
 	const controllerRef = useRef();
 	const dummyRef = useRef();
 	const [abortController, setAbortController] = useState(new AbortController());
-	const [visible, setVisible] = useState(false);
 
 	useEffect(() => {
 		// scroll to the end of the page when the user connects
@@ -58,36 +58,25 @@ const Chatroom = ({props}) => {
 				{/* // chat room */}
 				{messages && messages.map((message, index) =>
 					{
-						if(!message || !auth.currentUser) return(<></>);
-						let bDisplayUserPicture = (index !== 0 && messages[index-1].uid !== message.uid && message.uid !== auth.currentUser.uid)
-							|| (index === 0 && message.uid !== auth.currentUser.uid);
-						const date = new Date((message.createdAt?.seconds ? message.createdAt.seconds : firebase.firestore.FieldValue.serverTimestamp()) * 1000);
-						const formatedDate = `${date.toLocaleString()}`
-						// display the picture above the message if the first message is not from the current user or if a message comes after a message which is not his
-						if (bDisplayUserPicture) {
-							return (
-								<div className={message.uid === auth.currentUser.uid ? "right " : "left"} key={message.id + 'frag'}>
-									<div className={`${message.uid === auth.currentUser.uid ? "sent" : "received"} userTag`} key={message.id + 'tag'} style={{backgroundImage: `url("https://randomuser.me/api/portraits/men/${message.photoId}.jpg")`, backgroundPosition: "center", backgroundSize: "110%"}}>{message.allias}</div>
-									{!message.type && <div className={message.uid === auth.currentUser.uid ? "sent" : "received"} key={message.id} onMouseEnter={onHover} onMouseLeave={onLeave}>{message.text}</div>}
-									<div className={visible? "visible" : "hidden"}>TEST</div>
-									{/* {(message.type && message.type ==="image") && <div className={`${message.uid === auth.currentUser.uid ? "sent" : "received"} image`} key={message.id}><img src={message.text} alt="Base64 Image" /></div>} */}
-									{(message.type && message.type ==="image") && <div className={`${message.uid === auth.currentUser.uid ? "sent" : "received"} image`} key={message.id}><img className="displayedImage" src={message.text} alt="Base64 Image" /></div>}
-									<div className='timeStamp'key={message.id + "timeStamp"}>{formatedDate}</div>
-									{/* <div className={`${message.uid === auth.currentUser.uid ? "sent" : "received"} timeStamp`} key={message.id + "timeStamp"}>{formatedDate}</div> */}
-								</div>
-							)
-						} else {
-							return (
-								<div className={message.uid === auth.currentUser.uid ? "right " : "left"} key={message.id + 'frag'}>
-									{!message.type && <div className={message.uid === auth.currentUser.uid ? "sent" : "received"} key={message.id} onMouseEnter={onHover} onMouseLeave={onLeave}>{message.text}</div>}
-									{/* {(message.type && message.type ==="image") && <div className={`${message.uid === auth.currentUser.uid ? "sent" : "received"} image`} key={message.id}><img src={message.text} alt="Base64 Image" /></div>} */}
-									{(message.type && message.type ==="image") && <div className={`${message.uid === auth.currentUser.uid ? "sent" : "received"} image`} key={message.id}><img className="displayedImage" src={message.text} alt="Base64 Image" /></div>}
-									<div className={visible? "visible" : "hidden"}>TEST</div>
-									<div className={`timeStamp ${message.uid === auth.currentUser.uid ? "alignRight " : ""}`} key={message.id + "timeStamp"}>{formatedDate}</div>
-									{/* <div className={message.uid === auth.currentUser.uid ? "sent" : "received"} key={message.id + "timeStamp"}>{message.createdAt}</div> */}
-								</div>
-							)
-						}
+						// if(!message || !auth.currentUser) return(<></>);
+						// let bDisplayUserPicture = (index !== 0 && messages[index-1].uid !== message.uid && message.uid !== auth.currentUser.uid)
+						// 	|| (index === 0 && message.uid !== auth.currentUser.uid);
+						// const date = new Date((message.createdAt?.seconds ? message.createdAt.seconds : firebase.firestore.FieldValue.serverTimestamp()) * 1000);
+						// const formatedDate = `${date.toLocaleString()}`
+						// // display the picture above the message if the first message is not from the current user or if a message comes after a message which is not his
+						// return (
+						// 	<div className={message.uid === auth.currentUser.uid ? "right " : "left"} key={message.id + 'frag'}>
+						// 		{bDisplayUserPicture && <div className={`${message.uid === auth.currentUser.uid ? "sent" : "received"} userTag`} key={message.id + 'tag'} style={{backgroundImage: `url("https://randomuser.me/api/portraits/men/${message.photoId}.jpg")`, backgroundPosition: "center", backgroundSize: "110%"}}>{message.allias}</div>}
+						// 		{!message.type && <div className={message.uid === auth.currentUser.uid ? "sent" : "received"} key={message.id} onMouseEnter={onHover} onMouseLeave={onLeave}>{message.text}</div>}
+						// 		<div className={visible? "visible" : "hidden"}>TEST</div>
+						// 		{/* {(message.type && message.type ==="image") && <div className={`${message.uid === auth.currentUser.uid ? "sent" : "received"} image`} key={message.id}><img src={message.text} alt="Base64 Image" /></div>} */}
+						// 		{(message.type && message.type ==="image") && <div className={`${message.uid === auth.currentUser.uid ? "sent" : "received"} image`} key={message.id}><img className="displayedImage" src={message.text} alt="Base64 Image" /></div>}
+						// 		{/* <div className='timeStamp'key={message.id + "timeStamp"}>{formatedDate}</div> */}
+						// 		<div className={`timeStamp ${message.uid === auth.currentUser.uid ? "alignRight " : ""}`} key={message.id + "timeStamp"}>{formatedDate}</div>
+						// 		{/* <div className={`${message.uid === auth.currentUser.uid ? "sent" : "received"} timeStamp`} key={message.id + "timeStamp"}>{formatedDate}</div> */}
+						// 	</div>
+						// )
+						return <Message props={{messages, message, index}} key={message.id}></Message>
 					}
 				)}
 				<div ref={dummyRef}></div>
---
+++ src/components/Message.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useRef, useState } from 'react';
+import { auth } from '../firebaseConfig'
+
+const Message = ({props}) => {
+	const {messages, message, index} = props;
+	const [abortController, setAbortController] = useState(new AbortController());
+	const [visible, setVisible] = useState(false);
+	const timeoutId = useRef();
+
+	useEffect(() => {
+		const { signal } = abortController;
+
+		signal.addEventListener("abort", (event) => {
+			console.log("aborted: " + event.target.reason)
+			clearTimeout(timeoutId);
+		})
+	}, [abortController])
+	
+		//Source for the abort controller concept: https://www.youtube.com/shorts/VEdiHbjgIK4
+	
+		const onHover = () => {
+	
+			console.log("hoverd!");
+			const timeoutId = setTimeout(() => {
+				if (abortController && !abortController.signal.aborted) {
+					abortController.abort("time out completed");
+					setVisible(true);
+				}
+				setAbortController(new AbortController());
+			}, 800); // Increment the hover time every second
+		}
+	
+		const onLeave = () => {
+			console.log("leaved");
+			if (abortController && !abortController.signal.aborted) {
+				abortController.abort("leaved the element");
+				setAbortController(new AbortController());
+				setVisible(false);
+			}
+		}
+
+	if(!message || !auth.currentUser) return(<></>);
+	let bDisplayUserPicture = (index !== 0 && messages[index-1].uid !== message.uid && message.uid !== auth.currentUser.uid)
+		|| (index === 0 && message.uid !== auth.currentUser.uid);
+	const date = new Date((message.createdAt?.seconds ? message.createdAt.seconds : firebase.firestore.FieldValue.serverTimestamp()) * 1000);
+	const formatedDate = `${date.toLocaleString()}`
+	return (
+		<div className={message.uid === auth.currentUser.uid ? "right " : "left"} key={message.id + 'frag'}>
+			{/* display the picture above the message if the first message is not from the current user or if a message comes after a message which is not his */}
+			{bDisplayUserPicture && <div className={`${message.uid === auth.currentUser.uid ? "sent" : "received"} userTag`} key={message.id + 'tag'} style={{backgroundImage: `url("https://randomuser.me/api/portraits/men/${message.photoId}.jpg")`, backgroundPosition: "center", backgroundSize: "110%"}}>{message.allias}</div>}
+			{!message.type && <div className={message.uid === auth.currentUser.uid ? "sent" : "received"} key={message.id} onMouseEnter={onHover} onMouseLeave={onLeave}>{message.text}</div>}
+			<div className={visible? "visible" : "hidden"}>TEST</div>
+			{(message.type && message.type ==="image") && <div className={`${message.uid === auth.currentUser.uid ? "sent" : "received"} image`} key={message.id}><img className="displayedImage" src={message.text} alt="Base64 Image" /></div>}
+			<div className={`timeStamp ${message.uid === auth.currentUser.uid ? "alignRight " : ""}`} key={message.id + "timeStamp"}>{formatedDate}</div>
+		</div>
+	)
+}
+export default Message;
