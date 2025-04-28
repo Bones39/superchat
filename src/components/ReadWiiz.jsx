9a877cd724de7a1b984cb57c313645b1f63f2f6b
Andraina <asz_andraina@yahoo.fr>
2025-04-28 22:42:47 +0200

    clean import readWiiz section

--- src/components/ReadWiiz.jsx
+++ src/components/ReadWiiz.jsx
@@ -1,13 +1,11 @@
 import { auth, firestoreDb } from '../firebaseConfig';
-import { setDoc, collection, deleteDoc, doc, onSnapshot, updateDoc, where, deleteField } from 'firebase/firestore'
+import { doc, onSnapshot, updateDoc, deleteField } from 'firebase/firestore'
 import { useEffect, useState } from 'react';
 import { useSound } from 'use-sound';
 import wiizSound from '../assets/MSN_WIZZ_SOUND.mp3';
-import meowSound from '../assets/WIZZ_SOUND_MEOW_1.Mp3';
+import meowSound from '../assets/WIZZ_SOUND_MEOW_1.mp3';
 import wakeUpSound from '../assets/WIZZ_SOUND_WAKEUP.mp3';
 import { useRef } from 'react';
-import firebase from 'firebase/compat/app'
-import { useFormState } from 'react-dom';
 
 const ReadWiiz = ({readWiizProps}) => {
 
