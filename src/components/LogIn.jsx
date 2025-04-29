5a64f2afb0dfc47be117e00377622d037c2d9149
Andraina <asz_andraina@yahoo.fr>
2025-04-29 23:51:27 +0200

    WIP fetch cat pictures from login page

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,6 +1,27 @@
+import { useQuery } from '@tanstack/react-query';
 import logoImage from '../assets/LogoSuperChatCroped.png'
 
+// miaoullias
+// miaoussword
+// demiaou un compte?
+
+// create an account
+// sign in
+// https://thecatapi.com/
+
 const LogIn = ({props}) => {
+
+	const { data } = useQuery({
+		queryKey: ['logInCatImages'],
+		queryFn: async () => {
+			// using then statement leads to the following error: "Query data cannot be undefined. Please make sure to return a value other than undefined from your query function"
+			const response = await fetch("https://api.thecatapi.com/v1/images/search");
+			const data = await response.json();
+			console.log(data);
+			return data;
+		}
+	})
+
 	const {
 		email,
 		setEmail,
@@ -9,6 +30,7 @@ const LogIn = ({props}) => {
 		signIn,
 		signInWithGoogle
 	} = props;
+
 	return (
 		<div className="signInContainer">
 			<form className="signInFormContainer" onSubmit={signIn}>
@@ -19,6 +41,11 @@ const LogIn = ({props}) => {
 				<div className='logoSignIn'>
 					<p>Super</p><img id="imageLogo" src={logoImage} alt=''/><p>Chat</p>
 				</div>
+				{/* <div>
+					{data && data.map(element =>
+						<img id="logInCatImages" src={element.url} alt="" key={element.url}/>
+					)}
+				</div> */}
 			</form>
 		</div>
 	)
