ff2ef9800b6ee483540bc462c3feff29b37184ae
Andraina <asz_andraina@yahoo.fr>
2025-07-22 23:50:02 +0200

    Add more reaction smiley

--- src/components/Reactions.jsx
+++ src/components/Reactions.jsx
@@ -1,17 +1,24 @@
 import { useEffect, useRef, useState } from "react";
+import { MdOutlineQuestionAnswer } from "react-icons/md";
+import { FaCirclePlus } from "react-icons/fa6";
+import additionalSmileyArray from "../assets/additionnalReactionSmiley";
 
 const Reactions = ({props}) => {
 	const {displayReaction, selectingReaction, setSelectingReaction, selectedReaction, saveReaction} = props;
 	let fadingTimeoutId = useRef();
 	const [hovered, setHoverd] = useState('');
 	// -----------------------------------------------------------
-	const reactionArray = ['😄', '😥', '🤬', '🧡'];
+	const reactionArray = ['👍', '😄', '😥', '🤬', '🧡'];
 
 	const onHover = (currentSmiley) => {
 		clearTimeout(fadingTimeoutId);
 		setHoverd(currentSmiley);
 		// console.log(currentSmiley);
 		setSelectingReaction(true);
+	}
+
+	const openAdditionnalReactions = () => {
+
 	}
 
 	const onLeave = () => {
@@ -23,11 +30,16 @@ const Reactions = ({props}) => {
 	}
 
 	return (
-		<div>
+		<div id="reactionContainer">
 			<div className={`${(displayReaction || selectingReaction)? "" : "hidden"} horizontalLayout`} onMouseEnter={onHover} onMouseLeave={onLeave}>
 				{reactionArray.map((smiley) => <div key={smiley} className="smiley" onClick={()=>saveReaction(smiley)}>{smiley}</div>)}
+				<FaCirclePlus id="plusIcon" onClick={()=>console.log("ckicked!")}/>
+				{/* <MdOutlineQuestionAnswer size="1.7em"/> */}
 			</div>
 			<div>{selectedReaction}</div>
+			<div className={`${(displayReaction || selectingReaction)? "" : "hidden"} gridLayout`} onMouseEnter={onHover} onMouseLeave={onLeave}>
+				{additionalSmileyArray.map((smiley) => <div key={smiley} className="smiley" onClick={()=>saveReaction(smiley)}>{smiley}</div>)}
+			</div>
 		</div>
 	)
 }
